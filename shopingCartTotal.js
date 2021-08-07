dac233f4271431d8aab9b8d441e7ff0d53d7d419
rakibtweets <rakibhasanroky0@gmail.com>
2021-08-07 23:55:20 +0600

    3rd

--- shopingCartTotal.js
+++ shopingCartTotal.js
@@ -9,7 +9,7 @@ function totalPricing() {
     console.log('i am trying to use wsl it pera ditese')
     console.log('i am trying to use wsl it pera ditese')
     console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
+   
 
 
 }
