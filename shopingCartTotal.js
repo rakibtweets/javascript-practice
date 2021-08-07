a36719c365318b9946edf1e97fb3a1662b1a9313
rakibtweets <rakibhasanroky0@gmail.com>
2021-08-08 00:25:27 +0600

    added wsl link

--- shopingCartTotal.js
+++ shopingCartTotal.js
@@ -3,12 +3,12 @@
 function totalPricing() {
 
     console.log('hellow js');
-    console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
-    console.log('i am trying to use wsl it pera ditese')
+    console.log('i am trying to use wsl it pera ditese');
+    console.log('i am trying to use wsl it pera ditese');
+    console.log('i am trying to use wsl it pera ditese');
+    console.log('i am trying to use wsl it pera ditese');
+    console.log('i am trying to use wsl it pera ditese');
+    console.log('i am trying to use wsl it pera ditese');
    
 
 
